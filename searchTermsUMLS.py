1544ec575c7dad41d000d377c8669f4b511bafa7
Po-Yu Huang <poyu0987@gmail.com>
2019-10-16 00:34:47 -0400

    upload acronym expansions in UMLS, with the form of acronym:{expansion1, expansion2...}

--- searchTermsUMLS.py
+++ searchTermsUMLS.py
@@ -7,6 +7,7 @@
 
 from __future__ import print_function
 from Authentication import *
+import glob
 import requests
 import json
 import argparse
@@ -32,68 +33,44 @@ def search(string, apikey, searchType='exact'):
         results.append(result['name'])
     return results
 
+def _read_acronyms(acronyms_path):
+    """
+    read acronyms from txt file
+    return a gernerator
+    """
+    with open(acronyms_path, 'r') as f:
+        for line in f:
+            for i, char in enumerate(line):
+                if char.isdigit():
+                    yield line[:i-1]
+                    break
+
+def download_acronym_pairs(acronyms_path, apikey, path):
+    """
+    Download acronym pairs with the form of {'acronym1': 'meaning1',
+    'meaning2'...} every 10 acronyms.
+    """
+    d = defaultdict(list)
+    count = 1
+    for acronym in _read_acronyms(acronyms_path):
+        print(count, ': ', acronym)
+        for result in search(acronym, apikey):
+            d[acronym].append(result)
+        if count % 10 == 0:
+            with open('json/' + str(count) + path, 'w') as f:
+                json.dump(d, f)
+            d = defaultdict(list)
+        count += 1
+    with open('json/' + str(count) + path, 'w') as f:
+        json.dump(d, f)
+
+def merge_jsons(json_pattern):
+    d = {}
+    for file_path in glob.glob(json_pattern):
+        with open(file_path, 'r') as f:
+            d.update(json.load(f))
+    with open('acronym_expansions_UMLS.json', 'w') as f:
+        json.dump(d, f)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='process user given parameters')
-    #parser.add_argument("-u", "--username", required =  True, dest="username", help = "enter username")
-    #parser.add_argument("-p", "--password", required =  True, dest="password", help = "enter passowrd")
-    parser.add_argument("-k", "--apikey", required = True, dest = "apikey", help = "enter api key from your UTS Profile")
-    parser.add_argument("-v", "--version", required =  False, dest="version", default = "current", help = "enter version example-2015AA")
-    parser.add_argument("-s", "--string", required =  True, dest="string", help = "enter a search term, like 'diabetic foot'")
-
-    args = parser.parse_args()
-    #username = args.username
-    #password = args.password
-    apikey = args.apikey
-    version = args.version
-    string = args.string
-    uri = "https://uts-ws.nlm.nih.gov"
-    content_endpoint = "/rest/search/"+version
-    ##get at ticket granting ticket for the session
-    AuthClient = Authentication(apikey)
-    tgt = AuthClient.gettgt()
-    pageNumber=0
-    searchType = 'exact'
-
-    while True:
-        ##generate a new service ticket for each page if needed
-        ticket = AuthClient.getst(tgt)
-        pageNumber += 1
-        query = {'string':string,'ticket':ticket, 'pageNumber':pageNumber, 'searchType':searchType}
-        #query['includeObsolete'] = 'true'
-        #query['includeSuppressible'] = 'true'
-        #query['returnIdType'] = "sourceConcept"
-        #query['sabs'] = "SNOMEDCT_US"
-        r = requests.get(uri+content_endpoint,params=query)
-        r.encoding = 'utf-8'
-        items  = json.loads(r.text)
-        jsonData = items["result"]
-        #print (json.dumps(items, indent = 4))
-
-        print("Results for page " + str(pageNumber)+"\n")
-
-        for result in jsonData["results"]:
-
-          try:
-            print("ui: " + result["ui"])
-          except:
-            NameError
-          try:
-            print("uri: " + result["uri"])
-          except:
-            NameError
-          try:
-            print("name: " + result["name"])
-          except:
-            NameError
-          try:
-            print("Source Vocabulary: " + result["rootSource"])
-          except:
-            NameError
-
-          print("\n")
-
-        ##Either our search returned nothing, or we're at the end
-        if jsonData["results"][0]["ui"] == "NONE":
-            break
-        print("*********")
+    # download_acronym_pairs(acronyms_path, apikey, path)
