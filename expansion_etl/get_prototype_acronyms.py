0c3cc097aee3fa128cd73a14cba69514e0748192
Griffin Adams <griff4692@gmail.com>
2019-11-11 16:20:52 -0500

    Extraction plan for Columbia Discharge Summaries

---
+++ expansion_etl/get_prototype_acronyms.py
@@ -0,0 +1,24 @@
+import argparse
+import pandas as pd
+
+
+if __name__ == '__main__':
+    """
+    Get final ETL output for subset of acronyms (~10) on which we are developing our model.
+    """
+    parser = argparse.ArgumentParser(description='Get ETL Output for prototype acronyms only.')
+    args = parser.parse_args()
+
+    INPATH = './data/derived/merged_lf/'
+    acronyms = set(map(lambda x: x.strip(),
+        open('./data/original/prototype_acronyms.txt', 'r'))
+    )
+    df = None
+    for acronym in acronyms:
+        acronym_df = pd.read_csv('{}/{}.csv'.format(INPATH, acronym))
+        if df is None:
+            df = acronym_df
+        else:
+            df = pd.concat([df, acronym_df])
+
+    df.to_csv('./data/derived/prototype_acronym_expansions.csv', index=False)
