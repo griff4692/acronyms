2590aed059de7a09db5868ed6e8b0510ae0ce0f8
Griffin Adams <griff4692@gmail.com>
2019-11-13 09:36:30 -0500

    Successfully run context extractor on MIMIC and Columbia Discharge Summaries.

--- extract_dsums.py
+++ extract_dsums.py
@@ -1,49 +1,61 @@
+import json
+
 import pandas as pd
 
-from extract_context import ContextExtractor, ContextType
+from expansion_etl.extract_context import ContextExtractor, ContextType
 
 
-def get_all_contexts(context_extractor, doc_id, doc_string, sf, lfs):
-    contexts = []
-    forms = []
-    doc_ids = []
-
+def get_all_contexts(context_extractor, doc_id, doc_string, sfs, lfs):
+    contexts, doc_ids, forms = [], [], []
     config = {
         'type': ContextType.WORD,
         'size': 100
     }
 
-    for lf in lfs:
-        contexts += context_extractor.get_contexts_for_long_form(lf, doc_string, config, allow_inflections=True,
-                                                                 ignore_case=True)
-    contexts += context_extractor.get_contexts_for_short_form(sf, doc_string, config, allow_inflections=False,
-                                                              ignore_case=False)
+    for sf in sfs:
+        c = context_extractor.get_contexts_for_short_form(sf, doc_string, config, allow_inflections=False,
+                                                                  ignore_case=False)
+        forms += [sf] * len(c)
+        contexts += c
 
+    for lf in lfs:
+        c = context_extractor.get_contexts_for_long_form(lf, doc_string, config, allow_inflections=True,
+                                                                 ignore_case=True)
+        forms += [lf] * len(c)
+        contexts += c
     doc_ids += [doc_id] * len(contexts)
-    return zip(contexts, forms, doc_ids)
+    return list(zip(forms, doc_ids, contexts))
 
 
 if __name__ == '__main__':
     years = list(range(1988, 2015))
     context_extractor = ContextExtractor()
     contexts = []
-    doc_ids = []
+    df = pd.read_csv('./expansion_etl/data/derived/prototype_acronym_expansions.csv')
 
-    df = pd.read_csv('./data/derived/prototype_acronym_expansions_final.csv')
+    sfs = df['sf'].unique().tolist()
+    lfs = df['lf'].unique().tolist()
 
-    print(df.shape)
-
+    context_ct = 0
     for year in years:
+        print('Processing Columbia Discharge summaries in {}'.format(year))
         doc_string = ''
         start_line_idx = 0
-        file = open('/nlp/projects/medical_wsd/data/dsum_corpus_02/2014', 'r')
+        file = open('/nlp/projects/medical_wsd/data/dsum_corpus_02/{}'.format(year), 'r')
         for line_idx, line in enumerate(file):
             if line == '\n':
                 doc_id = '{}_{}'.format(line_idx, year)
-                contexts += list(get_all_contexts(context_extractor, doc_id, doc_string, sf, lfs))
-                doc_string = line
+                doc_contexts = get_all_contexts(context_extractor, doc_id, doc_string, sfs, lfs)
+                contexts += doc_contexts
+                context_ct += len(doc_contexts)
+                if len(doc_contexts) > 0:
+                    print('New Context Count={}'.format(context_ct))
+                doc_string = ''
                 start_line_idx = line_idx
             else:
                 doc_string += line
-
-        doc_string = ''
+    df = pd.DataFrame(contexts, columns=['form', 'doc_id', 'context'])
+    print('Saving {} contexts from Columbia discharge summaries.'.format(df.shape[0]))
+    df.to_csv('./expansion_etl/data/derived/columbia_prototype_contexts.csv', index=False)
+    json.dump(contexts, open('./expansion_etl/data/derived/columbia_prototype_contexts.json', 'w'))
+    print('Done processing Columbia discharge summaries!')
