1541c7ca972084c8461a771ca2b81965230fab5c
Griffin Adams <griff4692@gmail.com>
2019-11-22 18:15:45 -0500

    Resolve merge conflict.

--- expansion_etl/get_prototype_acronyms.py
+++ expansion_etl/get_prototype_acronyms.py
@@ -10,18 +10,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     INPATH = './data/derived/merged_lf/'
-<<<<<<< HEAD
-    df = pd.read_csv('./data/derived/standardized_acronym_expansions_inclusion_filtered.csv')
-
     acronyms = set(map(lambda x: x.strip(),
         open('./data/original/prototype_acronyms.txt', 'r'))
     )
 
-=======
-    acronyms = set(map(lambda x: x.strip(),
-        open('./data/original/prototype_acronyms.txt', 'r'))
-    )
->>>>>>> 0c3cc09... Extraction plan for Columbia Discharge Summaries
     df = None
     for acronym in acronyms:
         acronym_df = pd.read_csv('{}/{}.csv'.format(INPATH, acronym))
