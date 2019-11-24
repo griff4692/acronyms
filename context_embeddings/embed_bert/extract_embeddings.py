3a5a03f90d93189909e67546b6867eefd22fd23e
Griffin Adams <griff4692@gmail.com>
2019-11-24 09:55:27 -0500

    Add BERT code.

---
+++ context_embeddings/embed_bert/extract_embeddings.py
@@ -0,0 +1,30 @@
+import pickle
+import torch
+
+PROCESS_EMBED_LAYERS = {
+    'mean': torch.mean,
+    'first_layer': lambda x: x[0],
+    'last_layer': lambda x: x[-1],
+    'last_layers': lambda x: x[-4:].sum(0).data.numpy()
+}
+SELECTION_METHOD = 'last_layers'
+EMBEDDING_PATH = '/home/ga2530/acronyms/context_embeddings/data/embeddings.dict'
+def get_idx_to_embedding():
+    data = []
+    total_len = 0
+    with open(EMBEDDING_PATH, 'rb') as f:
+        while True:
+            try:
+                data.append(
+                    {x:PROCESS_EMBED_LAYERS[SELECTION_METHOD](y) for x, y in torch.load(f).items()}
+                )
+                total_len += 100
+            except EOFError:
+                break
+    idx_to_embedding = {}
+    for d in data:
+        idx_to_embedding.update(d)
+    return idx_to_embedding
+
+d = get_idx_to_embedding()
+torch.save(d, '/home/ga2530/acronyms/context_embeddings/data/embeddings_parsed.dict')
