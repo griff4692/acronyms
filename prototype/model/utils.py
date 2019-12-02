b86d9f3f04d26545836c9e051f739c276c514718
Griffin Adams <griff4692@gmail.com>
2019-12-02 16:35:03 -0500

    Add evaluation script.

--- prototype/model/utils.py
+++ prototype/model/utils.py
@@ -1,3 +1,10 @@
+import numpy as np
+
+
 def render_args(args):
     for arg in vars(args):
         print(arg, '-->', getattr(args, arg))
+
+
+def safe_multiply(a, b):
+    return np.exp(np.log(a + 1e-5) + np.log(b + 1e-5))
