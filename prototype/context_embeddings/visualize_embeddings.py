ceb335fe8669d1d6d0661ddb453c05cb4b934193
Griffin Adams <griff4692@gmail.com>
2019-12-09 17:20:15 -0500

    Create visualizations by SFs.

--- prototype/context_embeddings/visualize_embeddings.py
+++ prototype/context_embeddings/visualize_embeddings.py
@@ -1,6 +1,5 @@
 import os
 import matplotlib
-matplotlib.use('agg')
 import matplotlib.cm as cm
 import numpy as np
 import pickle
@@ -14,12 +13,13 @@ def extract_dims(embed_data):
     x, y = [], []
     for example in embed_data:
         x.append(float(example[0]))
-        x.append(float(example[1]))
+        y.append(float(example[1]))
     return x, y
 
 
 if __name__ == '__main__':
     in_fp = 'data/prototype_contexts_w_embeddings_reduced.pk'
+    print('Loading data')
     with open(in_fp, 'rb') as fd:
         data = pickle.load(fd)
 
@@ -43,6 +43,7 @@ if __name__ == '__main__':
             'lf_embeddings': defaultdict(list),
         }
 
+    print('Finished loading data.  Organizing data...')
     for idx, (form, embedding) in enumerate(zip(forms, embeddings)):
         if form in sfs:
             sf = form
@@ -52,10 +53,7 @@ if __name__ == '__main__':
             sf = lf_to_sf_map[form]
             sf_chart_data[sf]['lf_embeddings'][form].append(embedding)
 
-        if (idx + 1) % 10 == 0:
-            print('Processed {} out of {} examples'.format(idx + 1, len(forms)))
-            break
-
+    print('Now Plotting Charts loading data.  Organizing data...')
     for sf in sfs:
         plt.figure()
         this_lfs = list(sf_chart_data[sf]['lf_embeddings'].keys())
@@ -71,19 +69,16 @@ if __name__ == '__main__':
                 embeddings = sf_chart_data[sf]['lf_embeddings'][value]
 
             x, y = extract_dims(embeddings)
+            xs.append(x)
+            ys.append(y)
         for x, y, value, color in zip(xs, ys, values, colors):
-            plt.scatter(x, y, color=color)
+            plt.scatter(x, y, color=color, label=value)
 
-        plt.show()
-        break
+        plt.xlabel('First Component')
+        plt.ylabel('Second Component')
+        plt.title('Scatter of First Two Principal Components for {}'.format(sf))
+        plt.legend(bbox_to_anchor=(1.04,1), loc='upper left', fontsize='x-small')
 
-    # plt.figure()
-    # lf_index = self.embeddings_to_file_map[self.lf_count - 1][0] + self.embeddings_to_file_map[self.lf_count - 1][1]
-    # plt.scatter(reduced_embeddings[:lf_index, 0], reduced_embeddings[:lf_index, 1], c='r')  # For LFs
-    # plt.scatter(reduced_embeddings[lf_index:, 0], reduced_embeddings[lf_index:, 1], c='y')  # For SFs
-    # plt.xlabel('First Principal Component')
-    # plt.ylabel('Second Principal Component')
-    # plt.title('Scatter of First Two Principal Components for LFs and SFs')
-    # plt.legend(loc='upper left')
-    # # plt.show()
-    # plt.savefig('lf_and_sf_' + output_file_name + '.png')
+        out_png = 'visualizations/{}.png'.format(sf)
+        print('Saving image to {}'.format(out_png))
+        plt.savefig(out_png, bbox_inches='tight')
